ec6c41810908c6eac293479723f2e0004253eedb
imbarilyn <linahmuhonjaimbari@gmail.com>
2022-09-24 01:58:29 +0300

    added event listeners

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,7 @@ function Login() {
     <form onSubmit={handleSubmit}>
       <input type="text" name="username" placeholder="Enter username..." />
       <input type="password" name="password" placeholder="Enter password..." />
-      <button>Login</button>
+      <button type = "submit">Login</button>
     </form>
   );
 }
